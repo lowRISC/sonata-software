60365614d6939da92e495fcf752f4311207df220
Hugo McNally <hugo.mcnally@gmail.com>
2024-05-22 15:27:52 +0100

    Added a simple software setup

---
+++ compartments/echo.cc
@@ -0,0 +1,23 @@
+// Copyright lowRISC Contributors.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <compartment.h>
+//#include <debug.hh>
+#include <thread.h>
+#include <platform-uart.hh>
+
+/// Expose debugging features unconditionally for this compartment.
+//using Debug = ConditionalDebug<true, "led walk compartment">;
+
+/// Thread entry point.
+[[noreturn]]
+void __cheri_compartment("echo") entry_point()
+{
+	auto uart = MMIO_CAPABILITY(OpenTitanUart<>, uart);
+
+	char ch = '\n';
+	while (true) {
+		ch = uart->blocking_read();
+		uart->blocking_write(ch);
+	}
+}
---
+++ compartments/led_walk.cc
@@ -0,0 +1,33 @@
+// Copyright lowRISC Contributors.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <compartment.h>
+#include <debug.hh>
+#include <thread.h>
+#include <platform-gpio.hh>
+
+/// Expose debugging features unconditionally for this compartment.
+using Debug = ConditionalDebug<true, "led walk compartment">;
+
+static constexpr uint32_t num_leds = 8;
+
+/// Thread entry point.
+void __cheri_compartment("led_walk") start_walking()
+{
+	Debug::log("Look pretty LEDs!");
+
+	auto gpio = MMIO_CAPABILITY(SonataGPIO, gpio);
+
+	int count = 0;
+	bool switch_on = true;
+	while (true) {
+		if (switch_on) {
+			gpio->led_on(count);
+		} else {
+			gpio->led_off(count);
+		};
+		thread_millisecond_wait(500);
+		switch_on = (count == num_leds - 1) ? !switch_on : switch_on;
+		count = (count < num_leds - 1) ? count + 1 : 0;
+	}
+}
