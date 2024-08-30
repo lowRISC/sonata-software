adb7d84333436a82a15ca345f2318ab3c8f6ffcb
Hugo McNally <hugo.mcnally@gmail.com>
2024-08-30 10:46:41 +0100

    Moved the snake firmware image into it's xmake config

--- examples/snake/xmake.lua
+++ examples/snake/xmake.lua
@@ -4,3 +4,19 @@
 compartment("snake") 
   add_deps("lcd", "debug")
   add_files("snake.cc")
+
+firmware("snake_demo")
+    add_deps("freestanding", "snake")
+    on_load(function(target)
+        target:values_set("board", "$(board)")
+        target:values_set("threads", {
+            {
+                compartment = "snake",
+                priority = 2,
+                entry_point = "snake",
+                stack_size = 0x1000,
+                trusted_stack_frames = 2
+            }
+        }, {expand = false})
+    end)
+    after_link(convert_to_uf2)
--- examples/xmake.lua
+++ examples/xmake.lua
@@ -155,20 +155,3 @@ firmware("proximity_test")
         }, {expand = false})
     end)
     after_link(convert_to_uf2)
-
--- Snake demo
-firmware("snake_demo")
-    add_deps("freestanding", "snake")
-    on_load(function(target)
-        target:values_set("board", "$(board)")
-        target:values_set("threads", {
-            {
-                compartment = "snake",
-                priority = 2,
-                entry_point = "snake",
-                stack_size = 0x1000,
-                trusted_stack_frames = 2
-            }
-        }, {expand = false})
-    end)
-    after_link(convert_to_uf2)
