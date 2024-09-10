97c5f67198948898366eea4da8115305f8185cf9
Alex Jones <alex.jones@lowrisc.org>
2024-09-10 14:20:38 +0100

    Create UF2s for all software slots

--- common.lua
+++ common.lua
@@ -4,5 +4,7 @@
 function convert_to_uf2(target)
     local firmware = target:targetfile()
     os.execv("llvm-strip", { firmware, "-o", firmware .. ".strip" })
-    os.execv("uf2conv", { firmware .. ".strip", "-f0x6CE29E60", "-co", firmware .. ".uf2" })
+    os.execv("uf2conv", { firmware .. ".strip", "-b0x00000000", "-f0x6CE29E60", "-co", firmware .. ".slot1.uf2" })
+    os.execv("uf2conv", { firmware .. ".strip", "-b0x10000000", "-f0x6CE29E60", "-co", firmware .. ".slot2.uf2" })
+    os.execv("uf2conv", { firmware .. ".strip", "-b0x20000000", "-f0x6CE29E60", "-co", firmware .. ".slot3.uf2" })
 end
