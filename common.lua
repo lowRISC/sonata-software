9dfd8426e1143944ba29c6d2bbfbb371a5a1d870
Douglas Reis <doreis@lowrisc.org>
2025-04-28 15:44:48 +0100

    [xmake] Generate vmem for legacy builds
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- common.lua
+++ common.lua
@@ -8,3 +8,12 @@ function convert_to_uf2(target)
     os.execv("uf2conv", { firmware .. ".strip", "-b0x10000000", "-f0x6CE29E60", "-co", firmware .. ".slot2.uf2" })
     os.execv("uf2conv", { firmware .. ".strip", "-b0x20000000", "-f0x6CE29E60", "-co", firmware .. ".slot3.uf2" })
 end
+
+function convert_to_vmem(target)
+  local firmware = target:targetfile()
+  os.execv("riscv32-unknown-elf-objcopy", { firmware, "-O", "binary" , firmware .. ".bin" })
+  os.execv("srec_cat", { firmware .. ".bin", "-binary",
+                        "-offset" ,"0x0000", "-byte-swap", "4",
+                        "-o", firmware .. ".vmem", "-vmem" })
+end
+
